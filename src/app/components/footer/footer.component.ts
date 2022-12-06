ef5093c8b9039b86bba7afe41969e9b9d8794064
Jramos <jramos@saludelectronica.com>
2022-12-05 20:28:30 -0500

    Solucion de bugs

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class ComponentTenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ComponentTenComponent {
 
 }
