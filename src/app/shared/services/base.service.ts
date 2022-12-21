245c2bcc059cb97c80a5ddbe8a99324fd47bd373
Jramos <jramos@saludelectronica.com>
2022-12-21 17:28:23 -0500

    Cambio de la direccion de la api

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class BaseService {
-  private api = 'https://mentar-back-v3-production.up.railway.app/'
+  private api = 'http://ec2-3-95-176-155.compute-1.amazonaws.com:3000/'
   constructor(private http: HttpClient) {}
 
   postMethod(methodName: string, body: any) {
